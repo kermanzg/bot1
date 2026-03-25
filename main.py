bb2c3d6958bd293fa6b1b0c31295a18455f26e12
kermanzg <kermanzg@gmail.com>
2026-03-25 01:14:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -91,4 +91,6 @@ def webhook():
 # START SERVER (LOCAL)
 # ========================
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
